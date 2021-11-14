ca627a6c60fe74133af7afa5ae3519121702b207
floki1250 <d.adem1250@gmail.com>
2021-11-14 20:01:29 +0100

    first commit

---
+++ src/components/Background.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="main">
+    <div class="head"></div>
+    <div class="body"></div>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Background",
+});
+</script>
+<style lang="scss" scoped>
+.desktop {
+  background-color: rgba(0, 0, 0, 0.185);
+}
+@mixin dots($count) {
+  $text-shadow: ();
+  @for $i from 0 through $count {
+    $text-shadow: $text-shadow,
+      (-0.5+ (random()) * 3) +
+        em
+        (-0.5+ (random()) * 3) +
+        em
+        7px
+        hsla(random() * 360, 100%, 50%, 0.9);
+  }
+  text-shadow: $text-shadow;
+}
+
+.main {
+  font: 5vmin/1.3 Serif;
+  overflow: hidden;
+  background: red;
+}
+
+.body,
+.head {
+  display: block;
+  font-size: 52px;
+  color: rgba($color: #e41111, $alpha: 1);
+}
+
+.head::before,
+.head::after,
+.body::before,
+.body::after {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 3em;
+  height: 3em;
+  content: ".";
+  mix-blend-mode: screen;
+  animation: 44s -27s move infinite ease-in-out alternate;
+}
+
+.body::before {
+  @include dots(40);
+  animation-duration: 44s;
+  animation-delay: -27s;
+}
+
+.body::after {
+  @include dots(40);
+  animation-duration: 43s;
+  animation-delay: -32s;
+}
+
+.head::before {
+  @include dots(40);
+  animation-duration: 42s;
+  animation-delay: -23s;
+}
+
+.head::after {
+  @include dots(40);
+  animation-duration: 41s;
+  animation-delay: -19s;
+}
+
+div:first-line {
+  font-size: 300%;
+  font-style: italic;
+  letter-spacing: 0;
+  color: rgb(221, 0, 0);
+}
+
+@keyframes move {
+  from {
+    transform: rotate(0deg) scale(12) translateX(-20px);
+  }
+  to {
+    transform: rotate(360deg) scale(18) translateX(20px);
+  }
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,77 @@
+<template>
+  <Background />
+  <main>
+    <q-layout view="hHh lpR fFf">
+      <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="desktop">
+        <div class="drawer">
+          <q-list class="text-white">
+            <div class="item row">
+              <span></span>
+              <div class="row">
+                <q-icon name="print" />
+                <p style="">Home</p>
+              </div>
+            </div>
+            <div class="item row">
+              <span></span>
+              <p>Home</p>
+            </div>
+            <div class="item row">
+              <span></span>
+              <p>Home</p>
+            </div>
+            <div class="item row">
+              <span></span>
+              <p>Home</p>
+            </div>
+            <div class="item row">
+              <span></span>
+              <p>Home</p>
+            </div>
+          </q-list>
+        </div>
+      </q-drawer>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+
+      <q-footer reveal elevated v-model="footer" class="bg-grey-8 text-white">
+        <q-toolbar>
+          <q-toolbar-title>
+            <q-avatar>
+              <img
+                src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+              />
+            </q-avatar>
+            <div>Title</div>
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+    </q-layout>
+  </main>
+</template>
+
+<script>
+import { ref } from "vue";
+import Background from "src/components/Background.vue";
+
+export default {
+  components: { Background },
+  setup() {
+    const leftDrawerOpen = ref(true);
+    const footer = ref(false);
+
+    return {
+      leftDrawerOpen,
+      footer,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+      toggleFooter() {
+        footer.value = !footer.value;
+      },
+    };
+  },
+};
+</script>
