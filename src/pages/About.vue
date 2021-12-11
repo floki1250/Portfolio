3a6056fca4693f60fa6a0773ff4978f579c99908
Adem <37814393+floki1250@users.noreply.github.com>
2021-12-11 16:03:42 +0100

    11/12/21

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,25 @@
 <template>
-  <div class="item">About Me</div>
+  <q-page>
+    <div class="mini-box">
+      <p style="font-size: 30px; font-weight: 500">General Info</p>
+      <p>
+        I’m a Front-End / ERP Developer located in Tunisia. I currently am
+        working as a ERP Developer (JD Edwards Developer) at Innotech
+        Consulting.I am able to develop modules, reports and do administration,
+        I am also a software engineering student. I spend my free time putting
+        my creativity in developing tools and designing and I try to learn other
+        programming languages and other frameworks step by step.
+      </p>
+    </div>
+    <div class="mini-box">
+      <p style="font-size: 30px; font-weight: 500">Front End Development</p>
+      <p>
+        Front End Development is building out the visual components of a
+        website. Using Vue JS,Scss , and Javascript, I build fast, interactive
+        websites. This also may include a API's or other integrations. I am
+        accepting freelance work, but I'd love to get coffee and get to know
+        you, so don’t hesitate to get in touch.
+      </p>
+    </div>
+  </q-page>
 </template>
